9337e7ac6317c32b60eb23c33b92bb00cf042493
Mohan0416 <manomohan417@gmail.com>
2025-03-25 19:41:01 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Routes,Route} from 'react-router-dom'
-import Dashboard from './Pages/Dashboard.jsx'
 import Navbar from './Components/Navbar.jsx'
 import Transaction from "./Pages/Transaction.jsx";
 import Report from "./Pages/Report.jsx";
 import AddTransaction from './Pages/AddTransaction.jsx';
 import NotFound from './Pages/NotFound.jsx';
+import Dashboard from './Pages/Dashboard.jsx'
 
 const App = () => {
   return (
